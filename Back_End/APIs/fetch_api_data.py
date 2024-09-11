5d7be83a2bd7a9557c062b8b3ff9336de14414a8
hsfpu5h <helge.friedel@gmail.com>
2024-09-11 14:26:43 +0200

    cleaning errors

--- Back_End/APIs/fetch_api_data.py
+++ Back_End/APIs/fetch_api_data.py
@@ -1,6 +1,6 @@
 from weather_api import get_forecast
 
-def fetch_data(required_apis, city=None, state=None, country=None):
+def fetch_data(required_apis, location_query=None):
     results = {}
     for api in required_apis:
         if api == "weather_api":
--- Back_End/APIs/weather_api.py
+++ Back_End/APIs/weather_api.py
@@ -1,33 +1,11 @@
 from keys import API_WEATHERAPI_KEY
 from get_data_func import get_data
 
-'''def get_forecast(city, days=None):
-    """
-    constructs url to get forecast for certain days (3 default)
-    """
-    if not days:
-        days = 3
-
-    api_url = f"http://api.weatherapi.com/v1/forecast.json?key={API_WEATHERAPI_KEY}&q={city}&days={days}&alerts=yes"
-    forecast_data = get_data(api_url)
-
-    if forecast_data:
-        return forecast_data
-    else:
-        print("No data available")'''
-
-
 def get_forecast(location_query=0, days=None):
     """
-    Constructs the URL to get the weather forecast for a specific location (city, state, country)
-    and for a specified number of days.
+       constructs url to get forecast for certain days (3 default)
+       """
 
-    :param city: City name.
-    :param state: State name (optional).
-    :param country: Country name (optional).
-    :param days: Number of days for the forecast (1-10). Default is 3.
-    :return: Dictionary containing the forecast data.
-    """
     if not days:
         days = 3
 
