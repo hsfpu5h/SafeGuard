e68da6688c1ff4973de0ba6ebac9ee9f3b81c8da
Manvirsingh005 <manvirkooner05@gmail.com>
2024-09-11 13:50:25 +0200

    poll has been set up

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
+import atexit
+import requests
 from flask import Flask, render_template
+from apscheduler.schedulers.background import BackgroundScheduler
+def get_message():
+    response = requests.get("http://hackathons.masterschool.com:3030/team/getMessages/sgs")
+    data = response.json()
 
+    print(data)
+def init():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=get_message, trigger="interval", seconds=60)
+    scheduler.start()
+
+    # Shut down the scheduler when exiting the app
+    atexit.register(lambda: scheduler.shutdown())
+
+init()
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
